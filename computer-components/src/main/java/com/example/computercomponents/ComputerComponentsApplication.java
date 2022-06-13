24050002bbc0efbdb27fc96098b76d4e818cdad0
AleksicNikola00 <73480578+AleksicNikola00@users.noreply.github.com>
2022-06-13 02:24:49 +0200

    feat: Added swagger to project

--- computer-components/src/main/java/com/example/computercomponents/ComputerComponentsApplication.java
+++ computer-components/src/main/java/com/example/computercomponents/ComputerComponentsApplication.java
@@ -2,6 +2,12 @@ package com.example.computercomponents;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class ComputerComponentsApplication {
@@ -10,5 +16,16 @@ public class ComputerComponentsApplication {
 		SpringApplication.run(ComputerComponentsApplication.class, args);
 	}
 
+	@Configuration
+	public class SpringFoxConfig {
+		@Bean
+		public Docket api() {
+			return new Docket(DocumentationType.SWAGGER_2)
+					.select()
+					.apis(RequestHandlerSelectors.any())
+					.paths(PathSelectors.any())
+					.build();
+		}
+	}
 
 }
