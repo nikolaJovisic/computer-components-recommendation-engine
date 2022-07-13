7c7c758dba0f42b742bd51542795ee78f51377d9
AleksicNikola00 <73480578+AleksicNikola00@users.noreply.github.com>
2022-07-13 15:27:17 +0200

    feat: Created init frontend

--- front-end/component-frontend/src/app/app-routing.module.ts
+++ front-end/component-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecommandationComponent } from './app/page/recommandation/recommandation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'recommendation', component: RecommandationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front-end/component-frontend/src/app/app.module.ts
+++ front-end/component-frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './app/navbar/navbar.component';
+import { RecommandationComponent } from './app/page/recommandation/recommandation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    RecommandationComponent
   ],
   imports: [
     BrowserModule,
